b8f147a9e984100664a15cca62816f512bb63b8c
Adammm <adambahymaulana7@gmail.com>
2024-11-19 21:08:29 +0700

    push modifikasi PengunjungCafe01

--- PengunjungCafe01.java
+++ PengunjungCafe01.java
@@ -1,13 +1,16 @@
 public class PengunjungCafe01 {
-    public static void daftarPengunjung(String... namaPengunjung) {
+    public static void daftarPengunjung(Object... namaPengunjung) {
         System.out.println("Daftar Nama Pengunjung:");
-        for (int i = 0; i < namaPengunjung.length; i++) {
-            System.out.println("- " + namaPengunjung[i]);
-        }
+        for (Object object : namaPengunjung)
+            if (object instanceof String) {
+                System.out.println("nama - " + object);
+            } else if (object instanceof Integer) {
+                System.out.println("angka - " + object);
+            }
     }
 
     public static void main(String[] args) {
-        daftarPengunjung("Ali", "Budi", "Citra");
+        daftarPengunjung("Ali", 2, "Budi", 3, "Citra");
 
     }
 }
